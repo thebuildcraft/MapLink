818960f95bcc0185cdce19d1b9bde5ade9537063
thebuildcraft <thebuildcraft1@gmail.com>
2025-11-09 22:04:43 +0100

    version 4.1.2

--- common/src/main/java/de/the_build_craft/maplink/common/AbstractModInitializer.java
+++ common/src/main/java/de/the_build_craft/maplink/common/AbstractModInitializer.java
@@ -58,7 +58,7 @@ public abstract class AbstractModInitializer
 {
 	public static final String MOD_ID = "maplink";
 	public static final String MOD_NAME = "Map Link";
-	public static final String VERSION = "4.1.1";
+	public static final String VERSION = "4.1.2";
 	public static final Logger LOGGER = LogManager.getLogger("MapLink");
 	public static AbstractModInitializer INSTANCE;
 	public LoaderType loaderType;
